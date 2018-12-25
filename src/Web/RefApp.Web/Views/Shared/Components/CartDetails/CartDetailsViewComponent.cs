4d690312253b393db36e3bf699680f261e15d3ef
Teodor97 <brodo2g@gmail.com>
2018-12-25 15:52:13 +0200

    Fully implemented shopping cart.

---
+++ src/Web/RefApp.Web/Views/Shared/Components/CartDetails/CartDetailsViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RefApp.Web.Model.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RefApp.Web.Views.Shared.Components.CartDetails
+{
+    [ViewComponentAttribute]
+    public class CartDetailsViewComponent:ViewComponent
+    {
+        private readonly Cart cartService;
+
+        public CartDetailsViewComponent(Cart cartService)
+        {
+            this.cartService = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cartService);
+        }
+    }
+}
